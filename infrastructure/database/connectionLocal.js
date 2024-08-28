4364aff913e19672bb776d4db8d4918202349f1c
Tiavina22 <tiavinaramilison22.com>
2024-08-28 20:37:45 +0300

    Configuration the connection env prod

--- infrastructure/database/connectionLocal.js
+++ infrastructure/database/connectionLocal.js
@@ -2,9 +2,13 @@ require('../config/dotenv');
 
 const mysql = require('mysql2');
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`;
-
-const db = mysql.createConnection(urlDB);
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    connectTimeout: 30000 
+});
 
 db.connect((err) => {
     if (err) {
